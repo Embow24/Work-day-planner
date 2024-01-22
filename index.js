9b761ec6aa5fd7c549f40c379f4ff18867cd4397
Emily Kent <embow@Emilys-MacBook-Pro.local>
2024-01-22 13:55:36 +0000

    get data from local storage

--- index.js
+++ index.js
@@ -35,16 +35,14 @@ $('.row').each(function(){
    for(var i=0; i<hours.length; i++) {
     if (hourId === currentTime  ) {
         $(this).addClass("present")
-        $(this).removeClass("past")
-        $(this).removeClass("future")
+        $(this).removeClass("past, future")
     } else if (hourId < currentTime){
         $(this).addClass("past")
-        $(this).removeClass("present")
-        $(this).removeClass("future")
+        $(this).removeClass("present, future")
     } else{ 
         $(this).addClass("future")
-        $(this).removeClass("past")
-        $(this).removeClass("present")}
+        $(this).removeClass("past, present")
+    }
     }
 })
 }
@@ -63,4 +61,12 @@ localStorage.setItem(time, event)
 })
 
 //get data from local storage
-
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
